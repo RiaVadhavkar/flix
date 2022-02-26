1b418e77d9c295ed9f91cb7d630f55a7f774f104
Ria Vadhavkar <vadhavkar.ria@gmail.com>
2022-02-26 01:45:42 -0500

    Added Files to Repo

---
+++ FlixApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlixApp
+//
+//  Created by Ria Vadhavkar on 2/25/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("Hello")
+    }
+
+
+}
+
